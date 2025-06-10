c5c77acb32df464b08050e9c92705543a5503ac8
Bryyzxm <bryyzxm@gmail.com>
2025-06-10 18:07:28 +0700

    Add exp product column to product management and fix build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- output: 'export',
  images: {
   unoptimized: true,
  },
- basePath: '/Lie-ce',
- assetPrefix: '/Lie-ce/',
 };
 
 export default nextConfig;
