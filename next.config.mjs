f3b0adffbc8f239ce3c6cc51d426b1c76af2f1ec
Bryyzxm <bryyzxm@gmail.com>
2025-06-10 20:06:37 +0700

    Update next.config.mjs for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ output: 'export',
  images: {
   unoptimized: true,
  },
+ basePath: '/Lie-ce', // Replace with your repository name
+ assetPrefix: '/Lie-ce/', // Replace with your repository name
 };
 
 export default nextConfig;
