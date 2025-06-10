b0c376c5e0ec6ddefab946a23991ee3ba02e5871
Bryyzxm <bryyzxm@gmail.com>
2025-06-10 17:02:29 +0700

    feat: add GitHub Pages deployment configuration

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ output: 'export',
+ images: {
+  unoptimized: true,
+ },
+ basePath: '/Lie-ce',
+ assetPrefix: '/Lie-ce/',
+};
+
+export default nextConfig;
